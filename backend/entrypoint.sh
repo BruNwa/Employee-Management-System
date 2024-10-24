4e30b13ae64fea488d6ec4adb417197e8a7d11e2
Zaim Anwar <106646716+BruNwa@users.noreply.github.com>
2024-10-24 08:20:29 +0200

    +container debugging

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,8 +5,16 @@ while ! nc -z db 3306; do
   echo "Waiting for MySQL to be ready..."
   sleep 1
 done
-
 echo "MySQL is ready. Applying migrations..."
 flask db upgrade  
-echo "Starting the App..."
-python3 app.py
+echo "Starting flask app..."
+python3 app.py &
+FLASK_PID=$!  #get the app process id 
+while true; do
+    if ! kill -0 $FLASK_PID >/dev/null 2>&1; then
+        echo "the app has crached, the container is running for debugging."
+        break
+    fi
+    sleep 5
+done
+tail -f /dev/null
